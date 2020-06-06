cc29fcc6919503f42ebb2ccc6ddeb1114d4a0d35
madhumithaa <madhumithaa1411@gmail.com>
2020-06-06 12:50:38 +0530

    js started

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -3,3 +3,19 @@ function ToggleChatRoom(chatroom_id, chat_id) {
         $("#" + chat_id).toggleClass("d-none");
     });
 }
+
+// openchat
+function openChat() {
+
+    $('.left-box').css("display", "none");
+    $('.middle-box').css("display", "block");
+
+}
+
+// closechat
+function closeChat() {
+
+    $('.middle-box').css("display", "none");
+    $('.left-box').css("display", "block");
+
+}
